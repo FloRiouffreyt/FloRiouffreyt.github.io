99685bfd2b8e1fbba8b6e7903f3c56617ebe3042
BigDaddyFlo <112493915+FloRiouffreyt@users.noreply.github.com>
2023-04-12 20:38:45 +0200

    update tip calculator

--- 14_tip_calculator_app/script.js
+++ 14_tip_calculator_app/script.js
@@ -28,17 +28,73 @@ const inputTip = document.querySelector('#tip');
 const inputPeople = document.querySelector('#people');
 const inputPeopleError = document.querySelector('#people-error');
 
+const tipButtons = document.querySelectorAll('.tip-btn');
+
 inputBill.addEventListener('change', () => {
     displayErrorWithText(inputBill, inputBillError);
 })
 
 inputTip.addEventListener('change', () => {
     displayErrorWithoutText(inputTip);
+    for (let i = 0; i < tipButtons.length; i++) {
+        if (tipButtons[i].classList.contains('active')) {
+            tipButtons[i].classList.remove('active');
+        }
+    }
+    const customTip = document.querySelector('#tip').value / 100;
 })
 
 inputPeople.addEventListener('change', () => {
     displayErrorWithText(inputPeople, inputPeopleError);
 })
 
-//  TIPS CALCUTATIONS
+// BUTTONS ACTIVATED
 
+window.addEventListener('click', e => {
+    if (!e.target.matches('.tip-btn')) {
+        return false;
+    } else {
+        for (let i = 0; i < tipButtons.length; i++) {
+            if (tipButtons[i].classList.contains('active')) {
+                tipButtons[i].classList.remove('active');
+                e.target.classList.add('active');
+                inputTip.value = "";
+            } else {
+                e.target.classList.add('active');
+                inputTip.value = "";
+            }
+        }
+    }
+})
+
+// TIPS CALCUTATIONS
+
+const resultTip = document.querySelector('#result-tip');
+const resultTotal = document.querySelector('#result-total');
+
+// total tip = bill value * % of tip (e.g 10% tip for $100 bill => 100 * 0.1 = $10)
+// tip per person = total tip / nb of persons (e.g $10 / 2 = $5 each)
+// "tip amount per person" = (bill value * tip% ) / nb of persons
+// "total per person" = (bill value / nb of persons) + tip amount per person
+
+// const totalTip = inputBill.value * tipAmount;
+
+// RESET BUTTON
+
+const resetBtn = document.querySelector('#reset');
+
+resetBtn.addEventListener('click', () => {
+
+    inputBill.value = "";
+    inputTip.value = "";
+    inputPeople.value = "";
+    resultTip.innerHTML = "$0.00";
+    resultTotal.innerHTML = "$0.00";
+
+    for (let i = 0; i < tipButtons.length; i++) {
+        if (tipButtons[i].classList.contains('active')) {
+            tipButtons[i].classList.remove('active');
+        }
+    }
+
+})
