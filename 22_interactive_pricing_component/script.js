bf51fd1db27f1999133ae727f0ae607843b3a68f
BigDaddyFlo <112493915+FloRiouffreyt@users.noreply.github.com>
2023-05-03 15:38:54 +0200

    update slider

--- 22_interactive_pricing_component/script.js
+++ 22_interactive_pricing_component/script.js
@@ -1,9 +1,62 @@
 const billingCheck = document.querySelector('#billing-check');
 
+const viewField = document.querySelector('#views');
+const rateField = document.querySelector('#rate');
+const slider = document.querySelector('#range-slider');
+
+const views = [10, 50, 100, 500, 1];
+const rates = [8, 12, 16, 24, 36];
+var discountRates = rates;
+const discount = 0.75;
+
+function displayPlan(index, value = 'k') {
+    viewField.textContent = views[index] + value;
+    rateField.textContent = '$' + discountRates[index].toFixed(2);  
+}
+
+function valueSwitch() {
+    switch (slider.value) {
+        case '0':
+            displayPlan(0)
+            break;
+        case '25':
+            displayPlan(1)
+            break;
+        case '50':
+            displayPlan(2)
+            break;
+        case '75':
+            displayPlan(3)
+            break;
+        case '100':
+            displayPlan(4, 'm')
+            break;
+
+        default:
+            displayPlan(2)
+            break;
+    }
+}
+
 billingCheck.addEventListener('click', e => {
+    discountRates = []
     if (e.target.checked) {
         billingCheck.classList.add('checked')
+        for (let i = 0; i < rates.length; i++) {
+            discountRates.push(rates[i] * discount)        
+        }
     } else {
         billingCheck.classList.remove('checked')
+        for (let i = 0; i < rates.length; i++) {
+            discountRates.push(rates[i])        
+        }
     }
+    valueSwitch();
 })
+
+slider.addEventListener('change', () => {
+    valueSwitch();
+})
+
+window.onload = billingCheck.checked = false;
+window.onload = displayPlan(2);
