8bfdf1d414873977b8f6b431d871ac6ca51c49a8
BigDaddyFlo <112493915+FloRiouffreyt@users.noreply.github.com>
2023-05-05 15:59:06 +0200

    update dictionary

--- 23_dictionary_web_app/script.js
+++ 23_dictionary_web_app/script.js
@@ -1,112 +1,181 @@
 const searchWord = document.querySelector('#search-input');
 const searchBtn = document.querySelector('#search-validate');
+const searchError = document.querySelector('.error-text');
+const audioWrapper = document.querySelector('#audio');
+
+const noResult = document.querySelector('#error-message')
 
 const resultWord = document.querySelector('#word-result');
 const resultPhonetic = document.querySelector('#word-phonetic');
-const resultAudio = document.querySelector('#word-audio');
-const resultAudioBtn = document.querySelector('#word-audio-btn');
 
 const resultContent = document.querySelector('#results-content');
 
 const resultSources = document.querySelector('#results-sources');
 
-function wordResult() {
-    const word = searchWord.value;
+var previousWord = '';
 
+function emptyQuery() {
+    searchWord.classList.add('error');
+    searchError.classList.remove('hidden');
+}
+
+function resetDisplay() {
+    resultWord.textContent = '';
+    resultPhonetic.textContent = '';
+    resultContent.innerHTML = '';
+    resultSources.innerHTML = '';
+}
+
+function displayResult(resultData) {
+    !noResult.classList.contains('hidden') && noResult.classList.add('hidden');
+
+    resultWord.textContent = resultData.word;
+    resultPhonetic.textContent = resultData.phonetic;
+
+    for(let phonetic of resultData.phonetics) {
+        if (phonetic.audio !== '') {
+            audioWrapper.innerHTML = `
+            <button class="results__header_audio_btn" id="word-audio-btn">
+                <svg class="results__header_audio_btn-img" xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 75 75">
+                    <g fill="#A445ED" fill-rule="evenodd">
+                        <circle cx="37.5" cy="37.5" r="37.5" opacity=".25"/>
+                        <path d="M29 27v21l21-10.5z"/>
+                    </g>
+                </svg>
+            </button>
+            <audio src="${phonetic.audio}" id="word-audio"></audio>
+            `
+        } else if (phonetic.audio === '') {
+            audioWrapper.innerHTML = '';
+        }
+    }
+
+    const resultAudio = document.querySelector('#word-audio');
+    const resultAudioBtn = document.querySelector('#word-audio-btn');
+    
+    resultAudio && resultAudioBtn.addEventListener('click', () => {
+        resultAudio.play()
+    })
+
+    var output = '';
+
+    for(let meaning of resultData.meanings) {
+        output += `
+        <div>
+        <h2 class="results__content_part">${meaning.partOfSpeech}</h2>
+        <h3 class="results__content-meaning">Meaning</h3>
+        <ul class="results__content_list">
+        `
+        for(let definition of meaning.definitions) {
+            output += `
+            <li class="results__content_list-item">
+            <p>${definition.definition}</p>
+            ${definition.example !== undefined ? `<p>"${definition.example}"</p>` : ''}
+            </li>
+            `
+            }
+        output += '</ul>'
+
+        if (meaning.synonyms.length > 0) {
+            output += `
+            <p class="results__content_synonyms">Synonyms</p>
+            <ul class="results__content_synonyms_list">
+            `
+            for(let synonym of meaning.synonyms) {
+                output += `
+                <li class="results__content_synonyms_list-item">${synonym}</li>
+                `
+            }
+            output += '</ul>'
+        }
+        
+        if (meaning.antonyms.length > 0) {
+            output += `
+            <p class="results__content_antonyms">Antonyms</p>
+            <ul class="results__content_antonyms_list">
+            `
+            for(let antonym of meaning.antonyms) {
+                output += `
+                <li class="results__content_antonyms_list-item">${antonym}</li>
+                `
+            }
+            output += '</ul>'
+        }
+        output += '</div>'
+    }
+
+    resultContent.innerHTML = output;
+
+    var sourceOutput = `
+    <p>Source</p>
+    <ul>
+    `;
+    for(let source of resultData.sourceUrls) {
+        sourceOutput += `
+        <li>
+            <a href="${source}">${source}</a>
+        </li>
+        `
+    }
+    sourceOutput += '</ul>'
+
+    resultSources.innerHTML = sourceOutput;
+}
+
+function getResult(word) {
     fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+ word)
     .then(resp => {
-        return resp.json()
+        if (!resp.ok) {
+            resetDisplay();
+            noResult.classList.remove('hidden');
+            previousWord = word;
+            return false;
+        } else {
+            previousWord = word;
+            return resp.json()
+        }
     })
     .then(data => {
-        console.log(data[0]);
 
         const resultData = data[0];
 
-        resultWord.textContent = resultData.word;
-        resultPhonetic.textContent = resultData.phonetic;
-
-        resultAudio.setAttribute('src', '');
-        for(let phonetic of resultData.phonetics) {
-            if (phonetic.audio !== '') {
-                resultAudio.setAttribute('src', phonetic.audio)
-            }
+        if (resultData === undefined) {
+            return false;
+        } else {
+            displayResult(resultData);
         }
-        resultAudioBtn.addEventListener('click', () => {
-            resultAudio.play()
-        })
-
-        var output = '';
-
-        for(let meaning of resultData.meanings) {
-            output += `
-            <div>
-            <h2 class="results__content_part">${meaning.partOfSpeech}</h2>
-            <h3 class="results__content-meaning">Meaning</h3>
-            <ul class="results__content_list">
-            `
-            for(let definition of meaning.definitions) {
-                output += `
-                <li class="results__content_list-item">
-                <p>${definition.definition}</p>
-                ${definition.example !== undefined ? `<p>"${definition.example}"</p>` : ''}
-                </li>
-                `
-                }
-            output += '</ul>'
-
-            if (meaning.synonyms.length > 0) {
-                output += `
-                <p class="results__content_synonyms">Synonyms</p>
-                <ul class="results__content_synonyms_list">
-                `
-                for(let synonym of meaning.synonyms) {
-                    output += `
-                    <li class="results__content_synonyms_list-item">${synonym}</li>
-                    `
-                }
-                output += '</ul>'
-            }
-            
-            if (meaning.antonyms.length > 0) {
-                output += `
-                <p class="results__content_antonyms">Antonyms</p>
-                <ul class="results__content_antonyms_list">
-                `
-                for(let antonym of meaning.antonyms) {
-                    output += `
-                    <li class="results__content_antonyms_list-item">${antonym}</li>
-                    `
-                }
-                output += '</ul>'
-            }
-            output += '</div>'
-        }
-
-        resultContent.innerHTML = output;
-
-        var sourceOutput = `
-        <p>Source</p>
-        <ul>
-        `;
-        for(let source of resultData.sourceUrls) {
-            sourceOutput += `
-            <li>
-                <a href="${source}">${source}</a>
-            </li>
-            `
-        }
-        sourceOutput += '</ul>'
-
-        resultSources.innerHTML = sourceOutput;
     })
+}
+
+function wordResult() {
+    const word = searchWord.value;
+    
+    if (previousWord.toLowerCase() === word.toLowerCase()) {
+        return false;
+    } else {
+        searchWord.classList.contains('error') && searchWord.classList.remove('error');
+        !searchError.classList.contains('hidden') && searchError.classList.add('hidden');
+        
+        getResult(word);
+    }
 }
 
 searchWord.addEventListener('keydown', e => {
     if (e.key === "Enter") {
-        wordResult()
+        if (searchWord.value === '') {
+            emptyQuery();
+            return false;
+        } else {
+            wordResult()
+        }
     }
 })
 
 searchBtn.addEventListener('click', () => {
-    wordResult()
+    if (searchWord.value === '') {
+        emptyQuery();
+        return false;
+    } else {
+        wordResult()
+    }
 })
