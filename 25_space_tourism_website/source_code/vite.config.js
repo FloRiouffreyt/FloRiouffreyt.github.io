8803119a19ccf1fcdd29bb44c56e4a3f47c4053d
BigDaddyFlo <112493915+FloRiouffreyt@users.noreply.github.com>
2023-05-17 12:27:12 +0200

    Update vite.config.js

--- 25_space_tourism_website/source_code/vite.config.js
+++ 25_space_tourism_website/source_code/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/25_space_tourism_website",
+  base: "/25_space_tourism_website/",
   plugins: [react()],
 })
